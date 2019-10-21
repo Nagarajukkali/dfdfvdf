e4b416e6a1fb1f2f559db5f01ba04c95810b8118
Avilash Parida <avilash.parida@energyaustralia.com.au>
2019-10-21 14:54:19 +1100

    Implementation for testcafe cucumber html reporter

--- report-generator.js
+++ report-generator.js
@@ -1,22 +1,22 @@
-const report = require('multiple-cucumber-html-reporter');
-const path = require('path');
-const projectName = path.basename(__dirname);
-const projectVersion = process.env.npm_package_version;
-const reportGenerationTime = new Date().toISOString();
+const report = require('multiple-cucumber-html-reporter');
+const path = require('path');
+const projectName = path.basename(__dirname);
+const projectVersion = process.env.npm_package_version;
+const reportGenerationTime = new Date().toISOString();
 report.generate({
-  reportName: 'TestCafe Report',
-  jsonDir: 'sales-e2e/results',
-  reportPath: 'sales-e2e/results/html-reports',
-  openReportInBrowser: true,
-  disableLog: false,
-  displayDuration: true,
-  durationInMS: true,
-  customData: {
-    title: 'Run info',
-    data: [
-      { label: 'Project', value: `Energy Australia` },
-      { label: 'Release', value: `test` },
-      { label: 'Report Generation Time', value: `${reportGenerationTime}` },
-    ],
-  },
+  reportName: 'TestCafe Report',
+  jsonDir: 'sales-e2e/results/report',
+  reportPath: 'sales-e2e/results/html-reports',
+  openReportInBrowser: true,
+  disableLog: false,
+  displayDuration: true,
+  durationInMS: true,
+  customData: {
+    title: 'Run info',
+    data: [
+      { label: 'Project', value: `Energy Australia` },
+      { label: 'Release', value: `test` },
+      { label: 'Report Generation Time', value: `${reportGenerationTime}` },
+    ],
+  },
 });
--- sales-e2e/tests/step_definitions/sales.steps.js
+++ sales-e2e/tests/step_definitions/sales.steps.js
@@ -1,4 +1,4 @@
-const { Given, When, Then } = require('cucumber');
+const { Given, When, Then, Before } = require('cucumber');
 const { testData } = require('../../resources/resource')
 const EaHomePage = require('../pages/energy-australia-home.page');
 const { ClientFunction } = require('testcafe');
@@ -10,10 +10,11 @@ let cardName;
 const replace = { replace: true };
 
 Given('user have opened the website link in a browser', async t => {
+  await t.maximizeWindow();
   await t.navigateTo(EaHomePage.pageUrl);
 });
 
-And(/^user has navigated to '(.*)' plans page$/, async function(t, [customerType]) {
+Given(/^user has navigated to '(.*)' plans page$/, async function(t, [customerType]) {
   if(customerType==='Residential'){
     await t.click(EaHomePage.elements.redidentialComparePlansButton);
   }
@@ -25,7 +26,7 @@ When(/^user clicks on the verify modal window on '(.*)' page$/, async function(t
   }
 });
 
-And(/^user click on '(.*)' from the modal$/, async function(t, [modalWindowValue]) {
+When(/^user click on '(.*)' from the modal$/, async function(t, [modalWindowValue]) {
   if (modalWindowValue==='verify account') {
     await t.click(EaHomePage.elements.modalVerifyAccountOption);
   } else if (modalValue==='Bill upload') {
@@ -33,63 +34,91 @@ And(/^user click on '(.*)' from the modal$/, async function(t, [modalWindowValue
   }
 });
 
-Then(/^I enter valid account number and '(.*)' in Payment details section$/, async function(t, amountToPay) {
-  accountNumber = testData.accountNumber.substring(0, 4) + " " + testData.accountNumber.substring(4, 8) + " " + testData.accountNumber.substring(8, 10);
-  amount = amountToPay.toString();
-  await t
-    .typeText(PaymentPage.elements.accountNumber, testData.accountNumber, replace)
-    .typeText(PaymentPage.elements.amountToPay, amount, replace);
-});
-
-Then(/^I enter valid '(.*)' information in Payment method section$/, async function(t, cardType) {
-  const elements = PaymentPage.elements;
-  const type = cardType[0].charAt(0).toLowerCase() + cardType[0].substring(1);
-  cardName = cardType.toString();
-  const card = testData[type];
-
-  await t
-    .typeText(elements.creditCardNumber, card.cardNumber, replace)
-    .typeText(elements.nameOnCard, card.nameOnCard, replace)
-    .typeText(elements.expiryDate, dateFormat(cardName).toString(), replace)
-    .typeText(elements.cvv, card.cvv, replace);
-});
-
-Then(/^I am on '(.*)' page$/, async function(t, pageURLContent) {
-  const getPageUrl = ClientFunction(() => window.location.href);
-  await t.expect(getPageUrl()).contains(pageURLContent);
-});
-
-
-Then(/^I click '(.*)' button$/, async function(t, buttonName) {
-  await t.click(PaymentPage.elements.button.withText(buttonName.toString()));
-});
-
-
-Then('I see the account number and amount to pay prefilled correctly', async t => {
-  accountNumber = testData.smsParameters.accountNumber;
-  amount = testData.smsParameters.amount;
-  await t.expect(PaymentPage.elements.accountNumberPrefilled.textContent).contains(testData.smsParameters.accountNumber)
-    .expect(PaymentPage.elements.amountToPay.value).contains(amount);
-});
-
-Then('I see the spinner', async t => {
-  await t.expect(PaymentPage.elements.modalSpinner.visible).ok();
-});
-
-Then(/^I see the success message '(.*)'$/, async function(t, message) {
-  await t.expect(ReceiptPage.elements.successMessage.textContent).contains(message);
-});
-Then(/^I see the email success message '(.*)'$/, async function(t, message) {
-  await t.expect(ReceiptPage.elements.emailSuccessMessage.textContent).contains(message);
-});
-Then('I see the relevant payment receipt details', async t => {
-  if (cardName === 'Amex') {
-    cardName = 'AMEX'
+Then(/^user verifies if the verify account window is displayed as per the '(.*)' fueltype for '(.*)' customer$/, async function(t, [fuelType,customerType]) {
+  if (customerType==='Residential') {
+    switch (fuelType) {
+      case 'Both':
+        await t.expect(EaHomePage.elements.elecAccountNo.exists).ok;
+        await t.expect(EaHomePage.elements.gascAccountNo.exists).ok;
+        break;
+      case 'Elec':
+          await t.expect(EaHomePage.elements.elecAccountNo.exists).ok;
+        break;
+      case 'Gas':
+          await t.expect(EaHomePage.elements.gasAccountNo.exists).ok;
+        break;
+    }
+    await t.expect(EaHomePage.elements.postcodeVerifyAccount.exists).ok;
+    await t.expect(EaHomePage.elements.nextAccountNumber.exists).ok;
+    await t.expect(EaHomePage.elements.backAccountNumber.exists).ok;
+    //isElementDisplayedAndEnabled(ComparePlansPage_Resi.BackAccountNumber);
   }
-  await t.expect(ReceiptPage.elements.amount.textContent).contains(amount)
-    .expect(ReceiptPage.elements.accountNumber.textContent).contains(accountNumber)
-    .expect(ReceiptPage.elements.paidFrom.textContent).contains(cardName);
 });
-Then('I enter a valid email address', async t => {
-  await t.typeText(ReceiptPage.elements.emailAddress, testData.emailAddress);
+When(/^user provides '(.*)' account number '(.*)'$/, async function(t, [fuel, accountNumber]){
+  switch(fuel){
+    case 'Electricity':
+      await t.typeText(EaHomePage.elements.elecAccountNo, accountNumber, replace);
+      break;
+    case 'Gas':
+      await t.typeText(EaHomePage.elements.gasAccountNo,accountNumber, replace);
+      break;
+  }
+});
+
+When(/^user provides '(.*)' for '(.*)' customer$/, async function(t, [accountInformation, customerType]){
+  switch(customerType){
+    case 'Residential':
+      await t.typeText(EaHomePage.elements.postcodeVerifyAccount, accountInformation, replace);
+      break;
+    case 'Business':
+      await t.typeText(EaHomePage.elements.gascAccountNo,replace);
+      break;
+  }
+});
+
+When(/^user clicks on Next button after account number$/, async t => {
+  await t.click(EaHomePage.elements.nextAccountNumber);
+});
+
+When(/^User selects ID type '(.*)' and enters '(.*)'$/, async function(t, [idType, idValue]){
+  switch(idType){
+    case 'dob':
+        await t.click(EaHomePage.elements.idTypeDropDownVerifyAccount);
+        await t.click(EaHomePage.elements.idTypeDOBVerifyAccount);
+        await t.typeText(EaHomePage.elements.idTypeDOBValueVerifyAccount, idValue, replace);
+        break;
+    case 'dl':
+        await t.click(EaHomePage.elements.idTypeDropDownVerifyAccount);
+        await t.click(EaHomePage.elements.idTypeDlVerifyAccount);
+        await t.typeText(EaHomePage.elements.idTypeDlValueVerifyAccount, idValue, replace);
+        break;
+    case 'pin':
+        await t.click(EaHomePage.elements.idTypeDropDownVerifyAccount);
+        await t.click(EaHomePage.elements.idTypePinVerifyAccount);
+        await t.typeText(EaHomePage.elements.idTypeDlValueVerifyAccount, idValue, replace);
+        break;
+  }
+});
+
+Then(/^Usage data is displayed for '(.*)'$/, async function(t,[fuelType]){
+  switch (fuelType) {
+    case "Dual":
+        await t.expect(EaHomePage.elements.usageData_ele.exists).ok;
+        await t.expect(EaHomePage.elements.usageData_gas.exists).ok;
+      break;
+    case "Elec":
+        await t.expect(EaHomePage.elements.usageData_ele.exists).ok;
+      break;
+    case "Gas":
+        await t.expect(EaHomePage.elements.usageData_gas.exists).ok;
+      break;
+  }
+});
+
+When(/^user click show estimates on modal window$/, async t =>{
+  await t.click(EaHomePage.elements.getCostEstimatesChangeButton);
+});
+
+Then(/^User lands on plans page after verify account journey$/, async t =>{
+  await t.expect(EaHomePage.elements.changeLinkVerifyAccount).exists.ok;
 });
