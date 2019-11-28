7f690c8dff7cd6d922f0c8566c1f24577699eba1
Prateek <Prateek.Chauhan@energyaustralia.com.au>
2019-11-28 17:18:34 +1100

    no message

--- sales-e2e/tests/methods/checkoutCompletePage.ts
+++ sales-e2e/tests/methods/checkoutCompletePage.ts
@@ -1,8 +1,8 @@
 import {CustomerStatus, testFunction} from '../../global_methods/helper';
+const eaCheckoutCompletePage = require('../pages/checkoutComplete.page')
 
 export class checkoutCompleteMethod {
   public static async verifyWelcomePackMessage(t) {
-    await testFunction.isElementDisplayed(t, )
-    assertTrue(isElementPresent(CheckoutComplete_LifeSupportPage.lblWelcomeText));
+    await testFunction.isElementDisplayed(t, eaCheckoutCompletePage.elements.lblWelcomeText);
   }
 }
