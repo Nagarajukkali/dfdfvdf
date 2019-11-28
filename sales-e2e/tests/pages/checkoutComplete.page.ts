42282bb74fe575c77ef0704dfb8537d580395083
Prateek <Prateek.Chauhan@energyaustralia.com.au>
2019-11-28 17:19:13 +1100

    Added required locators for checkout complete page.

--- sales-e2e/tests/pages/checkoutComplete.page.ts
+++ sales-e2e/tests/pages/checkoutComplete.page.ts
@@ -2,7 +2,19 @@ import {Selector } from 'testcafe';
 
 const eaCheckoutCompletePage={
   elements: {
-
+    lblWelcomeText: Selector("[id*='WelText']"),
+    lifeSupportSubmitDisclaimer: Selector("#life-support-submit-disclaimer"),
+    linkDownloadForm: Selector("#life-support-submit-disclaimer a[href*='.pdf']"),
+    elecLifeSupport: Selector("#record-ele-life-support"),
+    iconElecLifeSupport: Selector("#ea-life-support-electricity-icon"),
+    lblElecLifeSupport: Selector("#record-ele-life-support-fuel-text"),
+    gasLifeSupport: Selector("#record-gas-life-support"),
+    iconGasLifeSupport: Selector("#ea-life-support-gas-icon"),
+    lblGasLifeSupport: Selector("#record-gas-life-support-fuel-text"),
+    lblElecLifeSupportStatus: Selector("#record-ele-life-support-status-text"),
+    lblGasLifeSupportStatus: Selector("#record-gas-life-support-status-text"),
+    listElecDevices: Selector("#record-ele-life-support-devices"),
+    listGasDevices: Selector("#record-gas-life-support-devices")
   }
 }
 
