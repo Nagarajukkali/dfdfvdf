e7471354cbf48f97bb8ca7f24a55add1a63f3f8e
Prateek <Prateek.Chauhan@energyaustralia.com.au>
2019-12-05 14:34:50 +1100

    step to navigate to my account

--- sales-e2e/tests/step_definitions/global.steps.ts
+++ sales-e2e/tests/step_definitions/global.steps.ts
@@ -2,6 +2,7 @@ import { Given } from 'cucumber';
 import {testFunction} from '../../global_methods/helper';
 import {CustomerType} from '@ea/ea-commons-models';
 const eaHomePage=require('../pages/energy-australia-home.page');
+const eaMyAccount = require('../pages/myAccount.page');
 
 
 Given(/^user has navigated to '(.*)' plans page$/, async function(t, [customerType]) {
@@ -11,4 +12,8 @@ Given(/^user has navigated to '(.*)' plans page$/, async function(t, [customerTy
     await testFunction.click(t, eaHomePage.elements.businessLink);
     await testFunction.click(t, eaHomePage.elements.smallBusinessButton);
   }
+});
+
+Given(/^user navigates to my account login page$/, async function (t, []) {
+  await t.navigateTo(eaMyAccount.pageUrl);
 });
