5aa2bfeb880ace84f9d44337933c7c09fbbc3c13
Prateek <Prateek.Chauhan@energyaustralia.com.au>
2019-12-05 16:51:53 +1100

    added steps and selectors

--- sales-e2e/tests/pages/myAccount.page.ts
+++ sales-e2e/tests/pages/myAccount.page.ts
@@ -6,7 +6,12 @@ const eaMyAccount={
   elements: {
     tfUsername: Selector("#username"),
     tfPassword: Selector("#password"),
-    btnSignIn: Selector("form[name*='loginForm'] button[type*='submit']")
+    btnSignIn: Selector("form[name*='loginForm'] button[type*='submit']"),
+    eleViewAndChangePlan: Selector("a[href*='view']"),
+    btnCompareAndSwitchPlans: Selector("[class*='header-action'] button"),
+    btnSolarNo: Selector("[class*='qualifier-question'] label[for*='edit-solar-panels-field-1']"),
+    btnQualifierConfirm: Selector("[primaryactiontext*='Confirm'] [class*='ea-button--primary']")
+
   },
 };
 
--- sales-e2e/tests/step_definitions/myAccount.steps.ts
+++ sales-e2e/tests/step_definitions/myAccount.steps.ts
@@ -3,8 +3,18 @@ import {testFunction} from '../../global_methods/helper';
 const eaMyAccount = require('../pages/myAccount.page');
 import {Crypto} from '../methods/Crypto';
 
-When(/^User logs in to my account using '(.*)' and '(.*)'$/, async function(t, [username, password]) {
+When(/^user logs in to my account using '(.*)' and '(.*)'$/, async function(t, [username, password]) {
   await testFunction.clearAndEnterText(t, eaMyAccount.elements.tfUsername, username);
   await testFunction.clearAndEnterText(t, eaMyAccount.elements.tfPassword, Crypto.decrypt(password));
   await testFunction.click(t, eaMyAccount.elements.btnSignIn);
+});
+When(/^user clicks on view and change plan accordion$/, async function (t, []) {
+  await testFunction.click(t, eaMyAccount.elements.eleViewAndChangePlan);
+});
+When(/^user clicks on compare and switch plan button$/, async function (t, []) {
+  await testFunction.click(t, eaMyAccount.elements.btnCompareAndSwitchPlans);
+});
+When(/^user selects No for solar question and confirm$/, async function (t, []) {
+  await testFunction.click(t, eaMyAccount.elements.btnSolarNo);
+  await testFunction.click(t, eaMyAccount.elements.btnQualifierConfirm);
 });
