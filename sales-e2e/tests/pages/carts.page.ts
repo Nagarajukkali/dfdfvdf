4ce3864ed18ee1fcf8929709deba95153d4cc238
Prateek <Prateek.Chauhan@energyaustralia.com.au>
2020-04-30 23:59:04 +1000

    adding selectors for ele features

--- sales-e2e/tests/pages/carts.page.ts
+++ sales-e2e/tests/pages/carts.page.ts
@@ -4,7 +4,11 @@ const cartsPage={
   elements: {
     cartsPageContinueButton: Selector('.cart-button-wrapper__text'),
     elePlanName: Selector("[id*='flick-cart-plan__header-fuel-ELE'] div[class*='title']"),
-    gasPlanName: Selector("[id*='flick-cart-plan__header-fuel-GAS'] div[class*='title']")
+    gasPlanName: Selector("[id*='flick-cart-plan__header-fuel-GAS'] div[class*='title']"),
+    eleFeature50Credit: Selector("[id*='flick-cart-plan__details-text-fuel-ELE']").withText("$50"),
+    eleFeatureCN: Selector("[id*='flick-cart-plan__details-text-fuel-ELE']").withText("Carbon neutral"),
+    eleFeatureDiscountOffTotal: Selector("[id*='flick-cart-plan__details-text-fuel-ELE']").withText("Discount off total energy bill"),
+
   },
 };
 
