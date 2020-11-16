0fe16cd961e323ae3f4ebf74fcf16e8c1b1c79a3
Ravi Pandey <ravi.pandey@energyaustralia.com.au>
2020-11-16 12:05:08 +1100

    updated the cart page selectors for cart page upgrade to angular 8

--- sales-e2e/tests/pages/carts.page.ts
+++ sales-e2e/tests/pages/carts.page.ts
@@ -2,25 +2,25 @@ import {Selector } from 'testcafe';
 
 const cartsPage={
   elements: {
-    cartsPageContinueButton: Selector('.cart-button-wrapper__text'),
-    elePlanName: Selector("[id*='flick-cart-plan__header-fuel-ELE'] div[class*='title']"),
-    gasPlanName: Selector("[id*='flick-cart-plan__header-fuel-GAS'] div[class*='title']"),
-    eleFeatures: Selector("[id*='flick-cart-plan__details-text-fuel-ELE']"),
-    eleFeature50Credit: Selector("[id*='flick-cart-plan__details-text-fuel-ELE']").withText("Credit"),
-    eleFeatureChanceToWin: Selector("[id*='flick-cart-plan__details-text-fuel-ELE']").withText("$2,000"),
-    eleFeatureCN: Selector("[id*='flick-cart-plan__details-text-fuel-ELE']").withText("Carbon neutral"),
-    eleFeatureDiscountOffTotal: Selector("[id*='flick-cart-plan__details-text-fuel-ELE']").withText(/Discount off total energy bill/i),
-    eleFeaturePeaceOfMind: Selector("[id*='flick-cart-plan__details-text-fuel-ELE']").withText("Peace of mind"),
-    eleFeatureDefaultOffer: Selector("[id*='flick-cart-plan__details-text-fuel-ELE']").withText("Default offer"),
-    eleFeatureVipPriorityService: Selector("[id*='flick-cart-plan__details-text-fuel-ELE']").withText("VIP Priority Service"),
+    cartsPageContinueButton: Selector('#flick-fix-button-wrapper__proceedBtn'),
+    elePlanName: Selector("#cart-plan-ELE .cart-plan__title"),
+    gasPlanName: Selector("#cart-plan-GAS .cart-plan__title"),
+    eleFeatures: Selector("#cart-plan-ELE .cart-plan__details-text"),
+    eleFeature50Credit: Selector("#cart-plan-ELE .cart-plan__details-text").withText("Credit"),
+    eleFeatureChanceToWin: Selector("#cart-plan-ELE .cart-plan__details-text").withText("$2,000"),
+    eleFeatureCN: Selector("#cart-plan-ELE .cart-plan__details-text").withText("Carbon neutral"),
+    eleFeatureDiscountOffTotal: Selector("#cart-plan-ELE .cart-plan__details-text").withText(/Discount off total energy bill/i),
+    eleFeaturePeaceOfMind: Selector("#cart-plan-ELE .cart-plan__details-text").withText("Peace of mind"),
+    eleFeatureDefaultOffer: Selector("#cart-plan-ELE .cart-plan__details-text").withText("Default offer"),
+    eleFeatureVipPriorityService: Selector("#cart-plan-ELE .cart-plan__details-text").withText("VIP Priority Service"),
 
-    gasFeatures: Selector("[id*='flick-cart-plan__details-text-fuel-GAS']"),
-    gasFeature50Credit: Selector("[id*='flick-cart-plan__details-text-fuel-GAS']").withText("Credit"),
-    gasFeatureChanceToWin: Selector("[id*='flick-cart-plan__details-text-fuel-GAS']").withText("$2,000"),
-    gasFeatureCN: Selector("[id*='flick-cart-plan__details-text-fuel-GAS']").withText("Carbon neutral"),
-    gasFeatureDiscountOffTotal: Selector("[id*='flick-cart-plan__details-text-fuel-GAS']").withText(/Discount off total energy bill/i),
-    gasFeaturePeaceOfMind: Selector("[id*='flick-cart-plan__details-text-fuel-GAS']").withText("Peace of mind"),
-    gasFeatureVipPriorityService: Selector("[id*='flick-cart-plan__details-text-fuel-GAS']").withText("VIP Priority Service")
+    gasFeatures: Selector("#cart-plan-GAS .cart-plan__details-text"),
+    gasFeature50Credit: Selector("#cart-plan-GAS .cart-plan__details-text").withText("Credit"),
+    gasFeatureChanceToWin: Selector("#cart-plan-GAS .cart-plan__details-text").withText("$2,000"),
+    gasFeatureCN: Selector("#cart-plan-GAS .cart-plan__details-text").withText("Carbon neutral"),
+    gasFeatureDiscountOffTotal: Selector("#cart-plan-GAS .cart-plan__details-text").withText(/Discount off total energy bill/i),
+    gasFeaturePeaceOfMind: Selector("#cart-plan-GAS .cart-plan__details-text").withText("Peace of mind"),
+    gasFeatureVipPriorityService: Selector("#cart-plan-GAS .cart-plan__details-text").withText("VIP Priority Service")
   },
 };
 
